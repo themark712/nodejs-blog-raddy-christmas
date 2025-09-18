38ad7ffdb15e551e39d1498a00aad4e518262b0e
cni-mark <mark.johnson@cnidist.com>
2025-09-18 07:18:00 -0500

    intial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website is running.
+          Website is running!
         </p>
         <a
           className="App-link"
